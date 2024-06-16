d9688b244afb2399acf32d9ab588fe3e5e832f38
syedimthiyaz <syedaala348@gmail.com>
2024-06-16 09:52:57 +0530

    added mongo

--- mongo.js
+++ mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/syed_imthiyaz';
+//const url = 'mongodb://localhost:27017/syed_imthiyaz';
+const url = 'mongodb+srv://syedimthiyaz348:syedimmu348@syedimthiyaz.ltxlzt9.mongodb.net/syed_imthiyaz'
 mongoose.connect(url);
 
 
