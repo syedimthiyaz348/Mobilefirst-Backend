79aa50fb5554499b62d51820a3f43e92139a1bbe
syedimthiyaz <syedaala348@gmail.com>
2024-06-16 10:33:15 +0530

    added env

--- mongo.js
+++ mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 //const url = 'mongodb://localhost:27017/syed_imthiyaz';
-const url = 'mongodb+srv://syedimthiyaz348:syedimmu348@syedimthiyaz.ltxlzt9.mongodb.net/syed_imthiyaz'
+//const url = 'mongodb+srv://syedimthiyaz348:syedimmu348@syedimthiyaz.ltxlzt9.mongodb.net/syed_imthiyaz'
+const url = process.env.MONGO_URL
 mongoose.connect(url);
 
 
